066badf9396a6f195dd48ad4271ab5fe23a5321c
jaglan1998 <bsinghjaglan@gmail.com>
2021-07-29 15:11:11 -0800

    Added a random computer player

--- Small Projects/tic_tac_toe/tic_tac_toe.py
+++ Small Projects/tic_tac_toe/tic_tac_toe.py
@@ -1,5 +1,6 @@
 # board is the list of 9 items where we can store the position of X or 0
 # outside the function all variables are global
+import random
 
 board = ['-', '-', '-',
          '-', '-', '-',
@@ -7,9 +8,16 @@ board = ['-', '-', '-',
 
 winner = None  # no one is winner yet
 game_is_going = True  # true as the game is on and no winner or tie yet
-current_player = input("\nChoose X or 0: ").upper()  # lets say the player is X
-while current_player not in ['X', '0']:
-    current_player = input("\nInvalid character. Choose X or 0: ").upper()
+
+computer_player = 'X'
+human_player = '0'
+print(f"Computer player is {computer_player} and human player is {human_player}.\n")
+print("The first turn is random computer or human.\n")
+current_player = random.choice([computer_player, human_player])
+
+# current_player = input("\nChoose X or 0: ").upper()  # lets say the player is X
+# while current_player not in ['X', '0']:
+#     current_player = input("\nInvalid character. Choose X or 0: ").upper()
 
 
 # displays the items stored in board list with help of indexing
@@ -18,22 +26,40 @@ def display_board():
     print(board[3] + '|' + board[4] + '|' + board[5])
     print(board[6] + '|' + board[7] + '|' + board[8])
 
+def empty_board():
+    if 'X' or "0" in board:
+        return True
+
+
+
+
 
 # to get the next valid move/position, displays that move on the board
 def next_move():
     global current_player, board
 
     while True:  # ask position, if not valid nest loop that part, if empty space draw value, if not loops back here
-        position = input("Choose a position from 1-9: ")  # we -1 to make it index as index start from 0
-        while position not in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:  # checks if input is valid or
-            position = input("Invalid character. Choose a position from 1-9: ")  # otherwise loops back
-        index = int(position)-1
-        if board[index] == '-':  # if the selected position is empty '-' only then we can
-            board[index] = current_player  # add current player e.g. 'X' on that
-            break  # every thing is fine, will add value on board
-        elif board[index] == 'X' or board[index] == '0':  # otherwise shows the try again message
-            print("\nAlready used spot, try again.....")
-            continue  # keep continue the loop until we get valid input and valid '-' blank space
+        if current_player == computer_player:
+            position = random.choice(['1', '2', '3', '4', '5', '6', '7', '8', '9'])
+            index = int(position) - 1
+            if board[index] == '-':  # if the selected position is empty '-' only then we can
+                board[index] = current_player  # add current player e.g. 'X' on that
+                break
+            elif board[index] == 'X' or board[index] == '0':  # otherwise shows the try again message
+                continue  # keep continue the loop until we get valid input and valid '-' blank space
+        else:
+            position = input("Choose a position from 1-9: ")  # we -1 to make it index as index start from 0
+            while position not in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:  # checks if input is valid or
+                position = input("Invalid character. Choose a position from 1-9: ")  # otherwise loops back
+            index = int(position) - 1
+            if board[index] == '-':  # if the selected position is empty '-' only then we can
+                board[index] = current_player  # add current player e.g. 'X' on that
+                break  # every thing is fine, will add value on board
+            elif board[index] == 'X' or board[index] == '0':  # otherwise shows the try again message
+                print("\nAlready used spot, try again.....")
+                continue  # keep continue the loop until we get valid input and valid '-' blank space
+
+
 
     board[index] = current_player
     return display_board()
@@ -101,10 +127,14 @@ def play_game():
     global current_player  # using global variable concept
     global game_is_going
 
-    display_board()  # step 1 - we just see the blank display board with '-'
+    if current_player == human_player and empty_board():
+        display_board()  # step 1 - we just see the blank display board with '-'
 
     while game_is_going:  # will keep looping while the game is going, which is true
-        print(f"\n...........Its' {current_player}'s turn.........\n")  # step 2 - tells the turn
+        if current_player == human_player:
+            print(f"\n...........Its' your turn.........\n")  # step 2 - tells the turn
+        else:
+            print("\n")
         next_move()  # step 3 - input valid move, displays it
         check_for_winner()  # step 4 - check the winner if any, then false the game_is_going
         flip_the_players()  # step 5 - if no winner flip the players to loop back again
