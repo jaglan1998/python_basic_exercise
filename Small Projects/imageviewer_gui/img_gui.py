7e6c2aa05c0091b65eb98b547730fb0c7ad4804b
jaglan1998 <bsinghjaglan@gmail.com>
2021-08-23 05:15:18 -0800

    Bugs fixed, status bar added

--- Small Projects/imageviewer_gui/img_gui.py
+++ Small Projects/imageviewer_gui/img_gui.py
@@ -2,58 +2,83 @@ from tkinter import *
 from PIL import ImageTk, Image
 
 root = Tk()
-root.title("Image Viewer")
-root.iconbitmap('img_gui.ico')
+root.title('Image Viewer')  # main root window title
+root.iconbitmap('img_gui.ico')  # adding icon
 
-f1 = LabelFrame(root, padx=10, pady=10)
-f2 = LabelFrame(root, padx=10, pady=10)
-f1.pack(padx=10, pady=10)
-f2.pack(padx=10, pady=10)
+f1 = LabelFrame(root)  # making frame for the photo
+f1.grid(row=0, column=0, columnspan=3)  # grid it with columnspan for 3 buttons
+
+# getting all the images
+img1 = ImageTk.PhotoImage(Image.open('img (1).jpg'))
+img2 = ImageTk.PhotoImage(Image.open('img (1).jfif'))
+img3 = ImageTk.PhotoImage(Image.open('img (2).jpg'))
+img4 = ImageTk.PhotoImage(Image.open('img (3).jpg'))
+img5 = ImageTk.PhotoImage(Image.open('img (3).jfif'))
+img6 = ImageTk.PhotoImage(Image.open('img (2).jfif'))
+imgList = [img1, img2, img3, img4, img5, img6]
+
+# show first image
+imgLabel = Label(f1, image=img1)
+imgLabel.grid(row=0, column=0, padx=20, pady=20, columnspan=3)
+
+# status bar
+imgStatus = Label(root, text=f'Image 1 of Image {len(imgList)} ', relief=SUNKEN, bd=1, anchor=E)
+imgStatus.grid(row=2, column=0, columnspan=3, sticky=EW)
 
 
-myImg1 = ImageTk.PhotoImage(Image.open('img (1).jpg'))
-myImg2 = ImageTk.PhotoImage(Image.open('img (2).jpg'))
-myImg3 = ImageTk.PhotoImage(Image.open('img (3).jpg'))
-myImg4 = ImageTk.PhotoImage(Image.open('img (1).jfif'))
-myImg5 = ImageTk.PhotoImage(Image.open('img (2).jfif'))
-myImg6 = ImageTk.PhotoImage(Image.open('img (3).jfif'))
-
-imgList = [myImg1, myImg2, myImg3, myImg4, myImg5, myImg6]
-img_index = 0
-myLabel = Label(f1, image=imgList[img_index])
-myLabel.grid(row=0, column=0)
-
-
-def button_back():
-    global img_index, myLabel, button_back
-    if img_index > 0:
-        img_index -= 1
-    # if img_index == 0:
-    #     button_back = Button(f2, text="<<", padx=15, pady=10, command=button_back, state=DISABLED).grid(row=1, column=0)
-    myLabel.grid_forget()
-    myLabel = Label(f1, image=imgList[img_index])
-    myLabel.grid(row=0, column=0)
+# button functions
+def back_b(img_no):
+    global b_exit, b_forward, b_back, imgLabel, imgStatus
+    imgLabel.grid_forget()  # just to forget last grid to avoid overlapping
+    imgLabel = Label(f1, image=imgList[img_no - 1])  # -1 to get the index of image
+    imgLabel.grid(row=0, column=0, padx=20, pady=20, columnspan=3)
+    # status bar change with the img no.
+    imgStatus = Label(root, text=f'Image {img_no} of Image {len(imgList)} ', relief=SUNKEN, bd=1, anchor=E)
+    imgStatus.grid(row=2, column=0, columnspan=3, sticky=EW)
+    # if we press the >> button again, index will +1
+    b_forward = Button(root, text='>>', padx=20, pady=20, command=lambda: forward_b(img_no + 1))
+    # if we press teh << button
+    b_back = Button(root, text='<<', padx=20, pady=20, command=lambda: back_b(img_no - 1))
+    if img_no == 1:  # if we are on first image
+        b_back = Button(root, text='<<', padx=20, pady=20, state=DISABLED)
+    # grid buttons
+    b_back.grid(row=1, column=0, padx=20, pady=20)
+    b_forward.grid(row=1, column=2, padx=20, pady=20)
     return
 
 
-def button_forward():
-    global img_index, myLabel, button_forward
-    if img_index < len(imgList)-1:
-        img_index += 1
-    if img_index == len(imgList)-1:
-        button_forward = Button(f2, text=">>", padx=15, pady=10, command=button_forward, state=DISABLED).grid(row=1, column=2)
-    else:
-        myLabel.grid_forget()
-        myLabel = Label(f1, image=imgList[img_index])
-        myLabel.grid(row=0, column=0)
+def forward_b(img_no):
+    global b_exit, b_forward, b_back, imgLabel, imgStatus
+    imgLabel.grid_forget()  # just to forget last grid to avoid overlapping
+    imgLabel = Label(f1, image=imgList[img_no - 1])  # -1 to get the index of image
+    imgLabel.grid(row=0, column=0, padx=20, pady=20, columnspan=3)
+    # status bar change with the img no.
+    imgStatus = Label(root, text=f'Image {img_no} of Image {len(imgList)} ', relief=SUNKEN, bd=1, anchor=E)
+    imgStatus.grid(row=2, column=0, columnspan=3, sticky=EW)
+    # if we press the >> button again, index will +1
+    b_forward = Button(root, text='>>', padx=20, pady=20, command=lambda: forward_b(img_no + 1))
+    if img_no == len(imgList):  # if we are on last image
+        b_forward = Button(root, text='>>', padx=20, pady=20, state=DISABLED)
+    # if we press teh << button
+    b_back = Button(root, text='<<', padx=20, pady=20, command=lambda: back_b(img_no - 1))
+    # grid buttons
+    b_back.grid(row=1, column=0, padx=20, pady=20)
+    b_forward.grid(row=1, column=2, padx=20, pady=20)
     return
 
-def button_exit():
+
+def exit_b():
     root.quit()
+    return
 
 
-button_back = Button(f2, text="<<", padx=15, pady=10, command=button_back).grid(row=1, column=0)
-button_forward = Button(f2, text=">>", padx=15, pady=10, command=button_forward).grid(row=1, column=2)
-button_exit = Button(f2, text="Exit", padx=15, pady=10, command=button_exit).grid(row=1, column=1)
+# defining buttons
+b_back = Button(root, text='<<', padx=20, pady=20, command=lambda: back_b(1), state=DISABLED)
+b_exit = Button(root, text='Exit', padx=20, pady=20, command=exit_b)
+b_forward = Button(root, text='>>', padx=20, pady=20, command=lambda: forward_b(2))
+# griding buttons
+b_back.grid(row=1, column=0, padx=20, pady=20)
+b_exit.grid(row=1, column=1, padx=20, pady=20)
+b_forward.grid(row=1, column=2, padx=20, pady=20)
 
 root.mainloop()
