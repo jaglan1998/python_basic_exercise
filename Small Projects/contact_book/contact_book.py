d5ffdb37f91300446c705468c830e5e7b985951d
jaglan1998 <bsinghjaglan@gmail.com>
2021-08-25 05:39:31 -0800

    used valid_inputs lib, made some new/modified/delete some functions to make the code simple and elegant.

--- Small Projects/contact_book/contact_book.py
+++ Small Projects/contact_book/contact_book.py
@@ -1,4 +1,6 @@
 import string
+import valid_inputs  # import whole folder
+from valid_inputs.valid_inputs import ValidInput  # import specific class
 
 
 class ContactsBook:
@@ -8,60 +10,30 @@ class ContactsBook:
         self.found_contact_index = None  # temp dump for found contact index, to delete or change
 
     @staticmethod
-    def valid_name():  # gets valid name
-        while True:  # keep on looping until we get valid name
-            name = input("Name: ")
-            temp = name.replace(' ', '')  # just mix first and last name
-            if len(temp) == 0:  # if we decide to pass the name
-                break
-            elif len(temp) > 20:
-                print("Name must be under 20 characters.")
-                continue
-            elif len(set(temp)-set(string.ascii_letters)) == 0:  # if the letters are english alphabets only
-                break
-            else:
-                print("Wrong input. Try again.")
-                continue
-        return name
-
-    @staticmethod
-    def valid_phone():
-        while True:  # loops until we get valid phone
-            phone = input("Phone: ")
-            try:  # trying to get the integral value
-                if len(phone) != 0:  # just to pass empty phone number
-                    ph = int(phone)
-            except ValueError:
-                print('Integral values only.')
-                continue
-            if len(phone) == 10 or len(phone) == 0:
-                break
-            print('Phone must be of 10 digits. Try again.')
-            continue
-        return phone
-
-    @staticmethod
     def add_contact(name, phone, address=None, notes=None):  # append all contacts into contacts.txt
         with open('contacts.txt', 'a') as cb:
             cb.write(f"{name}-{phone}-{address}-{notes}\n")
 
-    def generate_list(self):  # load the file, and makes lists and nested list
+    def import_data(self):  # load the file, and makes lists and nested list
         self.contact_list = []  # need to fresh restart with empty list
         with open('contacts.txt', 'r') as cb:
             contact_list = (cb.read().strip()).split('\n')  # each line in file is one contact separated with line
-            for contact in contact_list:  # each line in file has contact details separated with '-'
-                contact_details_list = contact.split('-')  # nested list with contact details (name, phone, ad, notes)
-                self.contact_list.append(contact_details_list)  # appending one nested list (one contact) after second.
+            # contact_list --> [contact 1, contact 2, contact 3, ....]
+            for contact in contact_list:  # each contact has name, ph, address, notes separated with '-'
+                contact_details_list = contact.split('-')
+                # contact_details_list --> [name, phone, address, notes]
+                self.contact_list.append(contact_details_list)
+                # self.contact_list --> [[[name, phone, address, notes], [name, phone, address, notes], ....]]
         return self.contact_list
 
-    def save_data(self):  # save data back into the file, for changing information
+    def export_data(self):  # save data back into the file, for changing information
         with open('contacts.txt', 'w') as cb:
             for contact in self.contact_list:  # looping each contact (nested list with all contact details)
-                cb.write('-'.join(contact) + '\n')  # join all contact details with '-' and write that in new line
+                cb.write('-'.join(contact) + '\n')  # join all contact details with '-' and write them in new line
 
     def show_contact_list(self):  # show complete list of contacts
-        print('-----------------------------------------------')
-        self.generate_list()  # refresh list
+        print('------------- CONTACT LIST --------------------')
+        self.import_data()  # refresh list
         for contact in self.contact_list:  # iterating each contact
             print(f"Name: {contact[0]}")
             print(f"Phone: {contact[1]}")
@@ -71,7 +43,7 @@ class ContactsBook:
 
     def find_contacts(self, name, phone):
         found = False  # just to know if we found or not found
-        self.generate_list()  # refresh the list for latest information
+        self.import_data()  # refresh the list for latest information
         print('-----------------------------------------------')
         for index, contact in enumerate(self.contact_list):  # iterate each contact and return index
             if name == contact[0] or phone == contact[1]:  # if any of these values match
@@ -88,83 +60,86 @@ class ContactsBook:
             print('-----------------------------------------------')
 
     def edit_contact(self, name, phone, address=None, notes=None):
-        self.generate_list()  # again refresh
+        self.import_data()  # again refresh
         self.found_contact[0] = name  # replacing the items in found contact list
         self.found_contact[1] = phone
         self.found_contact[2] = address
         self.found_contact[3] = notes
         self.contact_list[self.found_contact_index] = self.found_contact  # replacing the new contact in contact list
-        self.save_data()  # save the changed contact list into file
+        self.export_data()  # save the changed contact list into file
         print("Contact has been successfully edited.")
 
     def delete_contact(self):
-        self.generate_list()  # refresh again
+        self.import_data()  # refresh again
         self.contact_list.pop(self.found_contact_index)  # delete the found/selected contact
-        self.save_data()  # save info into the file
+        self.export_data()  # save info into the file
         print("Contact has been successfully deleted.")
 
 
-c = ContactsBook()
-while True:
-    print('-----------------------------------------------')
-    print("1. See complete contact list.")
-    print("2. Find a contact.")
-    print("3. Add contact.")
+# ---------------------------------------------------------------------------------------------------------------------
+if __name__ == "__main__":
+    def main_menu():
+        print('---------------- MAIN MENU ---------------------')
+        print("1. See complete contact list.")
+        print("2. Find a contact.")
+        print("3. Add contact.")
+        print("4. Quit.")
 
-    x = input("")
-    while x not in ['1', '2', '3']:
-        x = input("Wrong input. Try again.")
 
-    if x == '1':
-        c.show_contact_list()
-        x = input("Press enter to back.....")
-        while x != '':
-            x = input("Press enter to go back.....")
-        continue
-
-    elif x == '2':
-        name = c.valid_name()
-        phone = c.valid_phone()
-        c.find_contacts(name, phone)
-
+    def find_contacts_menu():
+        print('-----------------------------------------------')
         print('1. Edit.')
         print('2. Delete.')
         print('3. Main menu.')
         print('4. Quit.')
-        x = input("")
-        while x not in ['1', '2', '3', '4']:
-            x = input("Wrong input. Try again.")
         print('-----------------------------------------------')
 
-        if x == '1':
-            new_name = c.valid_name()
-            new_phone = c.valid_phone()
-            new_address = input("Address: ")
-            new_notes = input("Notes: ")
-            c.edit_contact(new_name, new_phone, new_address, new_notes)
-            print('-----------------------------------------------')
+    def start_program():
+        c = ContactsBook()  # creating a class instance
+        v = ValidInput()  # class instance
 
-        elif x == '2':
-            c.delete_contact()
-            x = input("")
-            while x not in ['1', '2', '3']:
-                x = input("Wrong input. Try again.")
-        elif x == '3':
-            continue
+        while True:
+            main_menu()  # 3 options available
+            x = v.get_valid_digit_input(1, 4)  # select valid
 
-        elif x == '4':
-            break
+            if x == '1':  # option to see
+                c.show_contact_list()  # complete contact list
+                e = v.valid_press_enter()  # asking for press enter to return to main menu
+                if e:  # if user press enter
+                    continue  # program start again from main menu
 
-    elif x == '3':
-        name = c.valid_name()
-        phone = c.valid_phone()
-        address = input("Address: ")
-        notes = input("Notes: ")
-        c.add_contact(name, phone, address, notes)
-        print('-----------------------------------------------')
-        x = input("Press enter to back.....")
-        while x != '':
-            x = input("Press enter to go back.....")
-        continue
+            elif x == '2':  # find contact
+                name = v.get_valid_name()
+                ph = v.get_valid_ph_no()
+                c.find_contacts(name, ph)  # find contact logic
+                find_contacts_menu()  # showing menu, 4 options available
+                y = v.get_valid_digit_input(1, 4)
+                if y == '1':  # option to edit contact
+                    print("**** ENTER NEW DETAILS ****")
+                    name = v.get_valid_name()
+                    ph = str(v.get_valid_ph_no())  # gets int, need str
+                    ad = input("Address: ")
+                    notes = input("Notes: ")
+                    c.edit_contact(name, ph, ad, notes)
+                elif y == '2':  # delete option
+                    c.delete_contact()
+                elif y == '3':  # return to main menu
+                    continue
+                elif y == '4':  # quit the program
+                    break
 
-input("Press any key to exit......")
+            elif x == '3':  # option to new contact
+                print("**** ENTER CONTACT DETAILS ****")
+                name = v.get_valid_name()
+                ph = v.get_valid_ph_no()
+                ad = input("Address: ")
+                notes = input("Notes: ")
+                c.add_contact(name, ph, ad, notes)
+                continue
+
+            elif x == '4':  # quit
+                break
+        return
+
+    start_program()
+    input("Press any key to exit.....")
